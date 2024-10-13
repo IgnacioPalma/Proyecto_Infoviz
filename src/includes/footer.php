fe7be25856b88054d1610781cd6e2424ab2039c4
IgnacioPalma <ignaciopalma.w@gmail.com>
2024-10-13 12:26:29 -0300

    add

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -4,7 +4,7 @@
                 <p class="col-md-4 mb-0 text-body-secondary instrument-font">Made with &hearts; by Grupo 25 for InfoVis</p>
 
                 <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary instrument-font">Disclaimer</a></li>
+                <li class="nav-item"><a href="/src/pages/disclaimer.php" class="nav-link px-2 text-body-secondary instrument-font">Disclaimer</a></li>
                 </ul>
             </footer>
         </div>
